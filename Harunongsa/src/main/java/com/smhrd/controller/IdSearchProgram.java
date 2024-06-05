32c243a8d8dbfe1156ea47fd95641934e1bd36bf
smhrd1 <smhrd1@smhrd>
2024-06-05 11:06:51 +0900

    커밋

---
+++ Harunongsa/src/main/java/com/smhrd/controller/IdSearchProgram.java
@@ -0,0 +1,27 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/IdSearchProgram")
+public class IdSearchProgram extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("[ID 찾기]");
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		
+		System.out.println("name : " + name);
+		System.out.println("email : " + email);
+		
+		
+	}
+
+}
