82b7f2813b54b6d51ba20c3268267394cbd398c9
smhrd1 <smhrd1@smhrd>
2024-06-05 17:50:58 +0900

    6월 5일 커밋

---
+++ Harunongsa/src/main/java/com/smhrd/model/KakaoDTO.java
@@ -0,0 +1,16 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class KakaoDTO {
+	private String id;
+	private String email;
+	
+	
+}
