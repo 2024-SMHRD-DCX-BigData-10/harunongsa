1bf43d8b7c0688f7b30fa3950d1445c7a5732cf2
smhrd1 <smhrd1@smhrd>
2024-06-07 16:39:11 +0900

    240607/김유빈/logincon->loginfail.html 주소 수정

--- Harunongsa/src/main/java/com/smhrd/controller/LoginCon.java
+++ Harunongsa/src/main/java/com/smhrd/controller/LoginCon.java
@@ -55,7 +55,7 @@ public class LoginCon extends HttpServlet {
          
       }else {
          System.out.println("로그인 실패");
-         moveURL = "LoginFail.jsp";
+         moveURL = "LoginFail.html";
       }
       
       response.sendRedirect(moveURL);
